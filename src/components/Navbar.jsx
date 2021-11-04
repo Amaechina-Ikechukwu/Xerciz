87ef5c6be2759a7814ed946e5168e762acc87a1c
Amaechina Ikechukwu <amaechinaikechukwu6@gmail.com>
2021-11-04 22:48:18 +0100

    still working on navbar, just ensuring i commit every change, so easy tracking

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,32 +1,36 @@
-import React, { Component } from 'react'
-import RunCircleOutlinedIcon from '@mui/icons-material/RunCircleOutlined';
-import AddOutlinedIcon from '@mui/icons-material/AddOutlined';
-import PersonAddOutlinedIcon from '@mui/icons-material/PersonAddOutlined';
-import { Link } from 'react-router-dom';
-import Tooltip, { tooltipClasses } from '@mui/material/Tooltip';
-import Typography from '@mui/material/Typography';
-import { styled } from '@mui/material/styles';
-import ModeEditOutlinedIcon from '@mui/icons-material/ModeEditOutlined';
-import "tailwindcss/tailwind.css"
-import { CollectionIcon, PencilIcon, PlusCircleIcon, UserCircleIcon } from '@heroicons/react/outline'
+import React, { Component } from "react";
+import RunCircleOutlinedIcon from "@mui/icons-material/RunCircleOutlined";
+import AddOutlinedIcon from "@mui/icons-material/AddOutlined";
+import PersonAddOutlinedIcon from "@mui/icons-material/PersonAddOutlined";
+import { Link } from "react-router-dom";
+import Tooltip, { tooltipClasses } from "@mui/material/Tooltip";
+import Typography from "@mui/material/Typography";
+import { styled } from "@mui/material/styles";
+import ModeEditOutlinedIcon from "@mui/icons-material/ModeEditOutlined";
+import "tailwindcss/tailwind.css";
+import {
+  CollectionIcon,
+  PencilIcon,
+  PlusCircleIcon,
+  UserCircleIcon,
+} from "@heroicons/react/outline";
 
 const LightTooltip = styled(({ className, ...props }) => (
-    <Tooltip {...props} classes={{ popper: className }} />
-  ))(({ theme }) => ({
-    [`& .${tooltipClasses.tooltip}`]: {
-      backgroundColor: theme.palette.common.white,
-      color: 'rgba(0, 0, 0, 0.87)',
-      boxShadow: theme.shadows[1],
-      fontSize: 11,
-    },
-  }));
-
+  <Tooltip {...props} classes={{ popper: className }} />
+))(({ theme }) => ({
+  [`& .${tooltipClasses.tooltip}`]: {
+    backgroundColor: theme.palette.common.white,
+    color: "rgba(0, 0, 0, 0.87)",
+    boxShadow: theme.shadows[1],
+    fontSize: 11,
+  },
+}));
 
 export class Navbar extends Component {
-    render() {
-        return (
-            <div>
-                {/* <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  render() {
+    return (
+      <div>
+        {/* <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">Navbar</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -88,55 +92,59 @@ export class Navbar extends Component {
   </div>
 </nav> */}
 
-<nav className="navbar navbar-dark bg-dark navbar-expand-lg">
-        <Link to="/" className="navbar-brand">ExcerTracker</Link>
-        <div className="collpase navbar-collapse">
-        <ul className="navbar-nav mr-auto">
-          <li className="navbar-item">
-          <Link to="/" className="nav-link">    <div class=' rounded-md p-0.5 hover:bg-gray-600  '>
-           <Tooltip title='Exercises'>
-        <li class="nav-item"><a class="nav-link active" aria-current="page" href="/">
-        
-             
-                <CollectionIcon class='w-5 h-5 text-gray-500 hover:text-gray-100' />
-           
-         </a>
-        </li> 
-        </Tooltip>
-        </div></Link>
-          </li>
-          <li className="navbar-item">
-          <Link to="/create" className="nav-link">       <div class=' rounded-md p-0.5 hover:bg-red-400  '>
-        <Tooltip title='Create Exercise'>
-        <li class="nav-item">
-          <a class="nav-link" href="/create">
-              
-                <PlusCircleIcon class='w-5 h-5 text-gray-500 hover:text-gray-100' />  
-              
-              </a>
-        </li>
-        </Tooltip>
-</div></Link>
-          </li>
-          <li className="navbar-item">
-          <Link to="/user" className="nav-link"><div class=' rounded-md p-0.5 hover:bg-blue-400'>
-        <Tooltip title='Exercise User'>
-        <li class="nav-item">
-          <a class="nav-link" href="/user">
-              
-                <UserCircleIcon class='w-5 h-5 text-gray-500 hover:text-gray-100' />  
-              
-              </a>
-        </li>
-        </Tooltip>
-</div></Link>
-          </li>
-        </ul>
-        </div>
-      </nav>
-            </div>
-        )
-    }
+        <nav className="navbar navbar-light bg-light navbar-expand-lg">
+          <Link to="/" className="navbar-brand">
+            Xerciz
+          </Link>
+          <div className="collpase navbar-collapse">
+            <ul className="navbar-nav mr-auto">
+              <li className="navbar-item">
+                <div class=" rounded-md p-0.5 hover:bg-gray-600  ">
+                  <Link to="/" className="nav-link">
+                    <Tooltip title="Exercises">
+                      <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="/">
+                          <CollectionIcon class="w-7 h-7 text-gray-500 hover:text-gray-100" />
+                        </a>
+                      </li>
+                    </Tooltip>
+                  </Link>
+                </div>
+              </li>
+
+              <li className="navbar-item">
+                <div class=" rounded-md p-0.5 hover:bg-red-400  ">
+                  {" "}
+                  <Link to="/create" className="nav-link">
+                    <Tooltip title="Create Exercise">
+                      <li class="nav-item">
+                        <a class="nav-link" href="/create">
+                          <PlusCircleIcon class="w-7 h-7 text-gray-500 hover:text-gray-100" />
+                        </a>
+                      </li>
+                    </Tooltip>
+                  </Link>
+                </div>
+              </li>
+              <li className="navbar-item">
+                <div class=" rounded-md p-0.5 hover:bg-blue-400">
+                  <Link to="/user" className="nav-link">
+                    <Tooltip title="Exercise User">
+                      <li class="nav-item">
+                        <a class="nav-link" href="/user">
+                          <UserCircleIcon class="w-7 h-7 text-gray-500 hover:text-gray-100" />
+                        </a>
+                      </li>
+                    </Tooltip>
+                  </Link>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </div>
+    );
+  }
 }
 
-export default Navbar
+export default Navbar;
